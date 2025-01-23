17803f2e1100059a95291cc2b1c748c0e8ce0e01
mFlaifel <msf-2oo7@hotmail.com>
2025-01-23 14:33:41 +0200

    wip

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,36 @@
 import { Plants } from '@/utils/dummyData';
 import { Text, View, Image, ScrollView } from 'react-native';
 import { Link } from 'expo-router';
+import { useState } from 'react';
 
 export default function App() {
+  const [plants, setPlants] = useState(Plants);
+
   return (
     <View>
-      <Text>Home Page1</Text>
-      <Link href='/cart'>
-        <Text>Go to Cart</Text>
-      </Link>
-
-      <ScrollView>
-        {Plants.map((item) => (
+      <View
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          padding: 10,
+        }}
+      >
+        <Text>Plants</Text>
+        <Link href='/cart'>
+          <Text>Cart </Text>
+        </Link>
+      </View>
+      <ScrollView
+        style={{
+          padding: 10,
+          display: 'flex',
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+          gap: 10,
+        }}
+      >
+        {plants.map((item) => (
           <View key={item.id}>
             <Text>{item.name}</Text>
             <Image
